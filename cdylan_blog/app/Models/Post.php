a8bc8db86b2b0bda7d84fa82a9ecf79da92ae85b
Dylan Corfmat <dylan.corfmat@gmail.com>
2022-11-21 16:31:36 -0500

    add specifity for posts related

---
+++ cdylan_blog/app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class Post extends Model
+{
+    use HasFactory, Notifiable;
+    protected $fillable = [
+        'title',
+        'slug',
+        'seo_title',
+        'excerpt',
+        'body',
+        'meta_description',
+        'meta_keywords',
+        'active',
+        'image',
+        'user_id',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
