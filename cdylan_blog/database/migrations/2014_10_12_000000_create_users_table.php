aa7f6293c6dfce5032df75d945dc50132fb1170c
Dylan Corfmat <dylan.corfmat@gmail.com>
2022-11-21 16:19:16 -0500

    add notifications, users and categories
    Migrations, Models, Factories if needed

--- cdylan_blog/database/migrations/2014_10_12_000000_create_users_table.php
+++ cdylan_blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', array('user', 'redac', 'admin'))->default('user');
+            $table->boolean('valid')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
