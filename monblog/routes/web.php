fea6790e078e1a0252501c500c5f169e848e6c7f
Dylan Corfmat <dylan.corfmat@gmail.com>
2022-11-22 16:50:22 -0500

    add laravel-filemanager

--- monblog/routes/web.php
+++ monblog/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use UniSharp\LaravelFilemanager\Lfm;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => 'auth'], function () {
+    Lfm::routes();
 });
 
 Route::get('/dashboard', function () {
