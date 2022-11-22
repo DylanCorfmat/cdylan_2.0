8929342fb0635a798c50cf679c0112422d60a581
Dylan Corfmat <dylan.corfmat@gmail.com>
2022-11-22 18:40:56 -0500

    add & delete comment

--- monblog/app/Models/User.php
+++ monblog/app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
 }
