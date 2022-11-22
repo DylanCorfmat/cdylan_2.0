d7f7638afc0e74c16274cbfdcc5c4d3ab4b50963
Dylan Corfmat <dylan.corfmat@gmail.com>
2022-11-22 11:38:29 -0500

    Data

--- cdylan/app/Models/User.php
+++ cdylan/app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'valid',
     ];
 
     /**
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
